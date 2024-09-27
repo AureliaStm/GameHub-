b74c609327a0b787d3dfa0153d86b61d747dbe7d
Aurelia Stm <aurelia.stamati@mail.ru>
2024-09-27 14:15:31 +0300

    Clean up the genres

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -5,10 +5,10 @@ interface Props {
     gameQuery: GameQuery;
 }
 const GameHeading = ({ gameQuery }: Props) => {
-    const heading = `${gameQuery.platform?.name || ''}${gameQuery.genre?.name || ''} Games`
+    const heading = `${gameQuery.platform?.name || ''} ${gameQuery.genre?.name || ''} Games`
     return (
         <Heading as='h1' marginY={5} fontSize='5xl'> {heading}</Heading >
     )
 }
 
-export default GameHeading
+export default GameHeading;
